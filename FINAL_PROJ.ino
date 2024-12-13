2febfe14867fa7c57b199b640645ce9e38d739a3
Alexandre Da Silva Pereira <140645929+souopreira@users.noreply.github.com>
2024-12-13 18:23:52 +0000

    Add files via upload

---
+++ FINAL_PROJ.ino
@@ -0,0 +1,259 @@
+#include "Arduino.h"
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <ESP32Servo.h>
+
+// Configurações do Display TFT
+#define TFT_CS   5
+#define TFT_DC   26
+#define TFT_MOSI 23
+#define TFT_MISO 19
+#define TFT_SCLK 18
+#define TFT_RST  -1 // Conectar ao 3V3
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST, TFT_MISO);
+
+// Pinos
+#define LDR_PIN 15
+#define LDR_PIN2 14
+#define SWITCH_1_POS1 34
+#define SWITCH_1_POS2 35
+#define SWITCH_2_POS1 19
+#define SWITCH_2_POS2 21
+#define LIGHT_PIN_1 32
+#define LIGHT_PIN_2 33
+#define LIGHT_PIN_3 2
+#define LIGHT_PIN_4 22
+#define POT_PIN 4
+#define SERVO_PIN_1 13
+#define SERVO_PIN_2 12
+#define SERVO_PIN_3 25
+#define SERVO_PIN_4 27
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+// Prototipacao das tarefas
+void Task1(void *pvParameters); // Atualizacao do grafico
+void Task3(void *pvParameters); // Logica principal do sistema
+void Task4(void *pvParameters); // Controle do servo
+void Task5(void *pvParameters); // Leitura do potenciometro
+
+// Variáveis e fila para comunicação entre tarefas
+QueueHandle_t queue;
+SemaphoreHandle_t xDisplaySemaphore;
+int state = 0; // Estados: 0 - OFF, 1 - ON, 2 - AUTO ON, etc.
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000); // Pequeno atraso para estabilizar
+
+  // Configuração de pinos
+  pinMode(LDR_PIN, INPUT);
+  pinMode(LDR_PIN2, INPUT);
+  pinMode(SWITCH_1_POS1, INPUT_PULLDOWN);
+  pinMode(SWITCH_1_POS2, INPUT_PULLDOWN);
+  pinMode(SWITCH_2_POS1, INPUT_PULLDOWN);
+  pinMode(SWITCH_2_POS2, INPUT_PULLDOWN);
+  pinMode(LIGHT_PIN_1, OUTPUT);
+  pinMode(LIGHT_PIN_2, OUTPUT);
+  pinMode(LIGHT_PIN_3, OUTPUT);
+  pinMode(LIGHT_PIN_4, OUTPUT);
+
+  // Inicializacao do servo e display
+  servo1.attach(SERVO_PIN_1);
+  servo2.attach(SERVO_PIN_2);
+  servo3.attach(SERVO_PIN_3);
+  servo4.attach(SERVO_PIN_4);
+
+  tft.begin();
+  tft.setRotation(2);
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(12, 15);
+  tft.println("Sistema de Farois");
+  tft.setCursor(50, 35);
+  tft.println("Direcionais");
+  tft.setCursor(55, 60);
+  tft.println("2024/2025");
+  tft.drawRect(5, 5, 230, 90, ILI9341_RED);
+  tft.drawRect(6, 6, 228, 88, ILI9341_RED);
+  tft.drawRect(3, 105, 232, 40, ILI9341_WHITE);
+
+
+  tft.drawCircle(30, 175, 26, ILI9341_BLUE);
+  tft.drawCircle(85, 175, 26, ILI9341_GREEN);
+
+  // Criacao da fila e tarefas
+  queue = xQueueCreate(10, sizeof(int));
+  xDisplaySemaphore = xSemaphoreCreateMutex();
+
+  if (queue != NULL && xDisplaySemaphore != NULL) {
+    xTaskCreate(Task1, "Task1", 4096, NULL, 2, NULL);
+    xTaskCreate(Task3, "Task3", 4096, NULL, 2, NULL);
+    xTaskCreate(Task4, "Task4", 4096, NULL, 1, NULL);
+    xTaskCreate(Task5, "Task5", 4096, NULL, 3, NULL);
+  } else {
+    Serial.println("EXIT CODE: NO_MEMORY");
+    while (1);
+  }
+}
+
+void loop() {
+  // O loop permanece vazio com FreeRTOS
+}
+
+// Implementacao da Task1
+void Task1(void *pvParameters) {
+  int lastAngleValue = 0; // Ultimo valor do angulo
+  for (;;) {
+    int angleValue = 0;
+    if (xQueueReceive(queue, &angleValue, portMAX_DELAY) == pdPASS) {
+      if (xSemaphoreTake(xDisplaySemaphore, portMAX_DELAY) == pdTRUE) {
+        // Limpar a barra anterior
+        if (lastAngleValue < 0) { // Limpar parte esquerda
+          int lastLeftLength = abs(lastAngleValue);
+          tft.fillRect(119 - lastLeftLength, 106, lastLeftLength, 38, ILI9341_BLACK); // Preenche com preto
+        } else { // Limpar parte direita
+          int lastRightLength = lastAngleValue;
+          tft.fillRect(119, 106, lastRightLength, 38, ILI9341_BLACK); // Preenche com preto
+        }
+
+        // Desenhar nova barra
+        if (angleValue < 0) { // Atualizar parte esquerda
+          int leftLength = abs(angleValue);
+          tft.fillRect(119 - leftLength, 106, leftLength, 38, ILI9341_GREEN); // Preenche com verde
+        } else { // Atualizar parte direita
+          int rightLength = angleValue;
+          tft.fillRect(119, 106, rightLength, 38, ILI9341_GREEN); // Preenche com verde
+        }
+
+        // Liberar semaforo apos atualizacao
+        xSemaphoreGive(xDisplaySemaphore);
+      }
+      // Atualizar o ultimo valor
+      lastAngleValue = angleValue;
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(100)); // Pequeno atraso para evitar execucao continua
+  }
+}
+
+// Implementacao da Task3
+void Task3(void *pvParameters) {
+  for (;;) {
+    int ldrValue = analogRead(LDR_PIN);
+    int ldrValue2 = analogRead(LDR_PIN2);
+    bool switch1 = digitalRead(SWITCH_1_POS1);
+    bool switch2 = digitalRead(SWITCH_1_POS2);
+    bool switch3 = digitalRead(SWITCH_2_POS1);
+    bool switch4 = digitalRead(SWITCH_2_POS2);
+
+    if (xSemaphoreTake(xDisplaySemaphore, portMAX_DELAY) == pdTRUE) {
+      // Determinacao do estado baseado nos sensores e switches
+      switch (state) {
+        case 1:
+        	tft.fillCircle(85, 175, 25, ILI9341_BLACK);
+          tft.fillCircle(30, 175, 25, ILI9341_BLUE);
+          digitalWrite(LIGHT_PIN_1, 1);
+          digitalWrite(LIGHT_PIN_2, 0);
+          servo3.write(35);
+          servo4.write(35);
+          Serial.print("\nSYSTEM ON");
+          break;
+        case 2:
+
+          tft.fillCircle(30, 175, 25, ILI9341_BLUE);
+          tft.drawChar(220, 165, 'A', ILI9341_WHITE, ILI9341_BLACK, 3);
+          digitalWrite(LIGHT_PIN_1, 1);
+          digitalWrite(LIGHT_PIN_2, 0);
+          servo3.write(35);
+          servo4.write(35);
+          Serial.print("\nSYSTEM AUTO ON");
+          break;
+        case 3:
+
+          tft.fillCircle(30, 175, 25, ILI9341_BLACK);
+          tft.fillCircle(85, 175, 25, ILI9341_BLACK);
+          tft.drawChar(220, 165, 'A', ILI9341_WHITE, ILI9341_BLACK, 3);
+          Serial.print("\nSYSTEM AUTO OFF");
+          digitalWrite(LIGHT_PIN_1, 0);
+          digitalWrite(LIGHT_PIN_2, 0);
+          servo3.write(35);
+          servo4.write(35);
+          break;
+        case 4:
+
+          tft.fillCircle(85, 175, 25, ILI9341_GREEN);
+          tft.drawChar(220, 165, 'A', ILI9341_WHITE, ILI9341_BLACK, 3);
+          Serial.print("\nSYSTEM AUTO MED");
+          digitalWrite(LIGHT_PIN_1, 0);
+          digitalWrite(LIGHT_PIN_2, 1);
+          servo3.write(75);
+          servo4.write(75);
+          break;
+        case 5:
+        	tft.fillCircle(30, 175, 25, ILI9341_BLACK);
+          tft.fillCircle(85, 175, 25, ILI9341_GREEN);
+          Serial.print("\nSYSTEM MED");
+          digitalWrite(LIGHT_PIN_1, 0);
+          digitalWrite(LIGHT_PIN_2, 1);
+          servo3.write(75);
+          servo4.write(75);
+          break;
+        default:
+        	tft.fillCircle(30, 175, 25, ILI9341_BLACK);
+        	tft.fillCircle(85, 175, 25, ILI9341_BLACK);
+        	tft.drawChar(220, 165, 'A', ILI9341_BLACK, ILI9341_BLACK, 3);
+          servo3.write(35);
+          servo4.write(35);
+          digitalWrite(LIGHT_PIN_1, 0);
+          digitalWrite(LIGHT_PIN_2, 0);
+          Serial.print("\nSYSTEM OFF");
+          break;
+      }
+      xSemaphoreGive(xDisplaySemaphore);
+    }
+
+    if (switch1) {
+      state = switch3 ? 1 : (switch4 ? 5 : state);
+    } else if (switch2) {
+      state = (ldrValue < 500) ? (switch4 ? 4 : (switch3 && ldrValue2 > 500 ? 4 : 2)) : 3;
+    } else {
+      state = 0;
+    }
+
+    Serial.println(ldrValue);
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+
+// Implementacao da Task4
+void Task4(void *pvParameters) {
+  for (;;) {
+    int angleValue = 0;
+    if (xQueueReceive(queue, &angleValue, portMAX_DELAY) == pdPASS) {
+      int servoAngle = map(angleValue, -115, 115, 0, 180);
+      servo1.write(servoAngle);
+      servo2.write(servoAngle);
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+
+// Implementacao da Task5
+void Task5(void *pvParameters) {
+  for (;;) {
+    int potValue = analogRead(POT_PIN);
+    int angleValue = map(potValue, 0, 4095, -115, 115);
+    if (xQueueSend(queue, &angleValue, portMAX_DELAY) == pdPASS) {
+      // Sucesso ao enviar
+    } else {
+      Serial.println("Erro: Não foi possível enviar para a fila!");
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
---
+++ sloeber.ino.cpp
@@ -0,0 +1,27 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2024-12-13 10:18:13
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <ESP32Servo.h>
+
+void setup() ;
+void loop() ;
+void Task1(void *pvParameters) ;
+void Task3(void *pvParameters) ;
+void Task4(void *pvParameters) ;
+void Task5(void *pvParameters) ;
+void vTemporaryTask(void *pvParameters) ;
+void vISR_Handler(void) ;
+void vManagerTask(void *pvParameters) ;
+
+#include "FINAL_PROJ.ino"
+
+
+#endif
